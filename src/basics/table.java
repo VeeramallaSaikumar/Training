ccd4ed2992959ef2c7c221214845a4bde20085e8
SaikumarVeeramalla <saikumarhg5472@gmail.com>
2025-01-17 18:02:19 +0530

    table -second Commit

---
+++ src/basics/table.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class table {
+    public static void main(String[] args) {
+//        System.out.print("Name ");
+//        System.out.print("Age ");
+//        System.out.println("Hobbies");
+//        System.out.print("Saikumar ");
+//        System.out.print("26 ");
+//        System.out.print("listening music");
+        System.out.printf("%-15s%-10s%-20s%n", "Name", "Age", "Hobby");
+        System.out.println("------------------------------------------");
+        System.out.printf("%-15s%-10d%-20s%n","saikumar",26,"Listening music");
+    }
+}
