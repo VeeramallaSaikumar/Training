32898fe05688f8c00b379c41414f60f45b0d8ee0
SaikumarVeeramalla <saikumarhg5472@gmail.com>
2025-01-17 15:14:23 +0530

    AgeName -FirstCommit

---
+++ src/basics/AgeName.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class AgeName {
+
+    public static void main(String[] args) {
+        System.out.println("My name is : saikumar");
+        System.out.println("My age is : 26");
+        System.out.println("My hobbies are : listening music");
+    }
+}
